7cb744f1ffb27f6e6226a8b984320420bfb5789d
Nicola Ricciardi <ricciardincl@gmail.com>
2025-12-04 02:27:46 +0100

    Fixes

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-SCENARIOS=("orbitalis-local") # ("local-async" "mqtt" "orbitalis-local" "orbitalis-mqtt" "local-multithread" "orbitalis-local-ff")
-N_WORKERS=(1 8)
-N_PRIMES=(5000) # (5000 10000 20000 30000 40000) 
+SCENARIOS=("local-async" "mqtt" "orbitalis-local" "orbitalis-mqtt" "local-multithread" "orbitalis-local-ff")
+N_WORKERS=(1 2 4 8)
+N_PRIMES=(5000 10000 20000 30000 40000) 
 ITERATIONS=(50)
-N_RUNS=2
+N_RUNS=10
 
 for iterations in "${ITERATIONS[@]}"; do
     for scenario in "${SCENARIOS[@]}"; do
